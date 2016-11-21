0848fd0ecddbc7f90679c18a94a34cdc5b19eaa8
Lu He <lu.he@avigilon.com>
2016-11-21 16:10:58 -0500

    Modify py_cpu_nms.py

--- nms/py_cpu_nms.py
+++ nms/py_cpu_nms.py
@@ -3,6 +3,8 @@
 # Copyright (c) 2015 Microsoft
 # Licensed under The MIT License [see LICENSE for details]
 # Written by Ross Girshick
+# TD-CNN
+# Modified by Huijuan Xu
 # --------------------------------------------------------
 
 import numpy as np
@@ -10,12 +12,10 @@ import numpy as np
 def py_cpu_nms(dets, thresh):
     """Pure Python NMS baseline."""
     x1 = dets[:, 0]
-    y1 = dets[:, 1]
-    x2 = dets[:, 2]
-    y2 = dets[:, 3]
-    scores = dets[:, 4]
+    x2 = dets[:, 1]
+    scores = dets[:, 2]
 
-    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    lengths = x2 - x1 + 1
     order = scores.argsort()[::-1]
 
     keep = []
@@ -23,13 +23,9 @@ def py_cpu_nms(dets, thresh):
         i = order[0]
         keep.append(i)
         xx1 = np.maximum(x1[i], x1[order[1:]])
-        yy1 = np.maximum(y1[i], y1[order[1:]])
         xx2 = np.minimum(x2[i], x2[order[1:]])
-        yy2 = np.minimum(y2[i], y2[order[1:]])
 
-        w = np.maximum(0.0, xx2 - xx1 + 1)
-        h = np.maximum(0.0, yy2 - yy1 + 1)
-        inter = w * h
+        inter = np.maximum(0.0, xx2 - xx1 + 1)
         ovr = inter / (areas[i] + areas[order[1:]] - inter)
 
         inds = np.where(ovr <= thresh)[0]
