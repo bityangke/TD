030c4ac30e4a3088f777856883b493a545dd24d0
Lu He <lu.he@avigilon.com>
2016-11-21 14:50:06 -0500

    Comment

--- tdcnn/twin_transform.py
+++ tdcnn/twin_transform.py
@@ -3,6 +3,8 @@
 # Copyright (c) 2015 Microsoft
 # Licensed under The MIT License [see LICENSE for details]
 # Written by Ross Girshick
+# TD-CNN
+# Modified by Huijuan Xu
 # --------------------------------------------------------
 
 import numpy as np
